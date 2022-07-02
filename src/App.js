0065691a8e9613f6d8e97d7fd9f016cab443d56b
zsline <sergei.dereka@gmail.com>
2022-07-02 08:08:38 +0300

    start messenger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
+import Messeges from './components/Messeges';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <Profile />
+      <Messeges />
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="App-navbar">
+      <div className='App-link'>
+        Профиль
+      </div>
+      <div className='App-link'>
+        Сообщения
+      </div>
+      <div className='App-link'>
+        Новости
+      </div>
+      <div className='App-link'>
+        Музыка
+      </div>
+      <div className='App-link setting'>
+        Настройки
+      </div>
+    </div>
+  )
+}
